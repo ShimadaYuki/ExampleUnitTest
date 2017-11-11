30e383a429b5546bdb44d6f39966a0fcec83930b
Shimada Yuki <e175750@ie.u-ryukyu.ac.jp>
2017-11-12 01:34:16 +0900

    commit clear

--- src/main/jp/ac/uryukyu/ie/e175750/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175750/Enemy.java
@@ -1,9 +1,20 @@
 package jp.ac.uryukyu.ie.e175750;
-
-
+/**
+ * 敵クラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
     public class Enemy extends LivingThing {
         public Enemy(String name, int maximumHP, int attack) {
             super(name, maximumHP, attack);
         }
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
     }
 
